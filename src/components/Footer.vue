0cb807069273ec324ba295a043c227abf55468b6
lucasbclls <luccasbclls2000@gmail.com>
2023-02-04 11:31:13 -0300

    Começo do projeto adicionado e delimitando espaços

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="footer">
+     <p>footer</p>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'footer'
+}
+
+</script>
+
+
+<style>
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="nav">
+    <router-link to="/">
+      <img src="logo" :alt="alt" id="logo" />
+    </router-link>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/pedidos">Pedidos</router-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  props: ["logo", "alt"],
+};
+</script>
+
+<style scoped>
+</style>
